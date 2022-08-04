9d1229ee7e11f72b716fd83a474fdf96e8f5ceaf
Dmytro Sabadash <dmitriysabadash1@gmail.com>
2022-08-04 22:53:39 +0300

    project template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './authentication/register/register.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { ConfirmEmailComponent } from './authentication/confirm-email/confirm-email.component';
+import { CrudComponent } from './users/crud/crud.component';
+import { UserComponent } from './users/user/user.component';
+import { FormComponent } from './users/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ConfirmEmailComponent,
+    CrudComponent,
+    UserComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
